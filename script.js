f622219a86c71f25de694eda3bcb019caa309485
borode <166508297+borode@users.noreply.github.com>
2024-08-07 01:36:22 +0300

    9 version  block two v.3

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ container.addEventListener('click', (e) => {
         e.target.nextElementSibling.classList.toggle('accordion_content--active');
     }
 })
+
+
+
+// const container_two = document.querySelector('.block_two_cards_text');
+// const container_three = container_two.querySelector('.block_two_cards_text_span');
+// console.log(container_two);
+// container_two.addEventListener("mouseover", function() {
+//     container_three.textContent ='Подробнее'
+// })
+// container_two.addEventListener("mouseout", function() {
+//     container_three.innerHTML ='Анализ Целевой Аудитории <div class="block_two_cards_after"></div>'
+//
+//
+// })
+//
+//
