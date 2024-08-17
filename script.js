3b08f699f323a2d8ade5eb22a73674e42df59d2a
borode <166508297+borode@users.noreply.github.com>
2024-08-17 14:58:56 +0300

    9 version  burger menu v 1.2

--- script.js
+++ script.js
@@ -12,4 +12,16 @@ container.addEventListener('click', (e) => {
     }
 })
 
+const burger = document.querySelector('.header_wrapper');
+const burgerTwo = document.querySelectorAll('.header_menu_burger');
+container.addEventListener('click', (e) => {
+    console.log(e.target);
+    if (e.target.closest('.header_menu_link_burger')) {
+        console.log('click');
+        for ( i = 0; i < items.length; i++ ) {
+            items[i].classList.remove('header_menu_burger--active');
 
+        }
+        e.target.nextElementSibling.classList.toggle('header_menu_burger--active');
+    }
+})
