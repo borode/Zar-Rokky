fefae2faf355a85d1e2626d280c9ac7b1c8648e6
borode <166508297+borode@users.noreply.github.com>
2024-08-19 01:15:15 +0300

    9 version  pop_up windows v 1

--- script.js
+++ script.js
@@ -12,17 +12,55 @@ container.addEventListener('click', (e) => {
     }
 })
 
-const burger = document.querySelector('.header_wrapper');
+const burger = document.querySelector('.header_menu_link_burger');
+const menu = document.querySelector('.header_menu_burger');
+const menuLinks = document.querySelectorAll(".header_menu_link_burger_hidden");
 burger.addEventListener('click', (e) => {
     console.log(e.target);
     if (e.target.closest('.header_menu_link_burger')) {
         console.log('click');
-        for ( i = 0; i < items.length; i++ ) {
-            items[i].classList.remove('header_menu_burger--active');
+        burger.classList.toggle('header_menu_link_burger--active');
+        menu.classList.toggle('header_menu_burger--active');
 
+    }
+    menuLinks.forEach((lease) =>
+        lease.addEventListener("click", () => {
+            menu.classList.remove("header_menu_burger--active");
+            burger.classList.remove("header_menu_link_burger--active");
+        })
+    );
+
+})
+const wrapper = document.querySelector('body');
+wrapper.addEventListener('click', (e) => {
+    if (!e.target.closest('.header_menu_link_burger')){
+        console.log('click');
+        if (menu.classList.contains('header_menu_burger--active')&& burger.classList.contains('header_menu_link_burger--active')) {
+            menu.classList.remove("header_menu_burger--active");
+            burger.classList.remove("header_menu_link_burger--active");
         }
-        e.target.nextElementSibling.classList.toggle('header_menu_burger--active');
-
     }
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
